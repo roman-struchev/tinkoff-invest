7994a094ca4790eb34bc74ecd9a690a1035339e8
Struchev Roman Konstantinovich <struchev-rk@fxclub.org>
2022-05-12 08:12:04 +0200

    observe listener errors

--- src/main/java/com/struchev/invest/service/candle/CandleListenerService.java
+++ src/main/java/com/struchev/invest/service/candle/CandleListenerService.java
@@ -29,40 +29,39 @@ public class CandleListenerService {
     private final ITinkoffCommonAPI tinkoffCommonAPI;
     private final NotificationService notificationService;
 
-    private void startToListen() {
+    private void startToListen(int number) {
         var figies = strategySelector.getFigiesForActiveStrategies();
-        tinkoffCommonAPI.getApi().getMarketDataStreamService()
-                .newStream("candles_stream", item -> {
-                    log.trace("New data in streaming api: {}", item);
-                    if (item.hasCandle()) {
-                        var candle = HistoricCandle.newBuilder();
-                        candle.setClose(item.getCandle().getClose());
-                        candle.setOpen(item.getCandle().getOpen());
-                        candle.setHigh(item.getCandle().getHigh());
-                        candle.setLow(item.getCandle().getLow());
-                        candle.setTime(item.getCandle().getTime());
-                        var candleDomainEntity = candleHistoryService.addOrReplaceCandles(candle.build(), item.getCandle().getFigi());
-                        purchaseService.observeNewCandleNoThrow(candleDomainEntity);
-                    }
-                }, e -> log.error("candles_stream error", e))
-                .subscribeCandles(new ArrayList<>(figies));
-    }
-
-    private void startToListenInThread(int number) {
-        new Thread(() -> {
-            try {
-                notificationService.sendMessageAndLog("Listening candle events..");
-                startToListen();
-            } catch (Throwable th) {
-                log.error("An error, listener will be restarted", th);
-                startToListenInThread(number + 1);
-                throw th;
-            }
-        }, "event-listener-" + number).start();
+        notificationService.sendMessageAndLog("Listening candle events..");
+        try {
+            tinkoffCommonAPI.getApi().getMarketDataStreamService()
+                    .newStream("candles_stream", item -> {
+                        log.trace("New data in streaming api: {}", item);
+                        if (item.hasCandle()) {
+                            var candle = HistoricCandle.newBuilder();
+                            candle.setClose(item.getCandle().getClose());
+                            candle.setOpen(item.getCandle().getOpen());
+                            candle.setHigh(item.getCandle().getHigh());
+                            candle.setLow(item.getCandle().getLow());
+                            candle.setTime(item.getCandle().getTime());
+                            var candleDomainEntity = candleHistoryService.addOrReplaceCandles(candle.build(), item.getCandle().getFigi());
+                            purchaseService.observeNewCandleNoThrow(candleDomainEntity);
+                        }
+                    }, e -> {
+                        log.error("An error in candles_stream, listener will be restarted", e);
+                        startToListen(number + 1);
+                    })
+                    .subscribeCandles(new ArrayList<>(figies));
+        } catch (Throwable th) {
+            log.error("An error in subscriber, listener will be restarted", th);
+            startToListen(number + 1);
+            throw th;
+        }
     }
 
     @PostConstruct
     void init() {
-        startToListenInThread(1);
+        new Thread(() -> {
+            startToListen(1);
+        }, "event-listener").start();
     }
 }
