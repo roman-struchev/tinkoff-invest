378d98b4251ae97410e992629123b7b6ab68bf20
Roman Struchev <underpender@yandex.ru>
2024-04-14 16:21:12 +0200

    change strategies

--- src/main/java/com/struchev/invest/strategy/instrument_by_instrument/CNYbyUSDbyHKDbyKZTbyGoldBySilverStrategyDP045.java
+++ src/main/java/com/struchev/invest/strategy/instrument_by_instrument/CNYbyUSDbyHKDbyKZTbyGoldBySilverStrategyDP045.java
@@ -6,11 +6,15 @@ import org.springframework.stereotype.Component;
 import java.util.Map;
 
 @Component
-public class BYNbyKZTStrategy extends AInstrumentByInstrumentStrategy {
+public class CNYbyUSDbyHKDbyKZTbyGoldBySilverStrategyDP045 extends AInstrumentByInstrumentStrategy {
 
     private final Map<String, Integer> FIGIES = new ImmutableMap.Builder<String, Integer>()
-            .put("BBG00D87WQY7", 3000) // BYN
+            .put("BBG0013HRTL0", 7000) // CNY
+            .put("BBG0013HGFT4", 1000) // USD
+            .put("BBG0013HSW87", 8000) // HKD
             .put("BBG0013HG026", 400000) // KZT
+            .put("BBG000VJ5YR4", 14)   // золото
+            .put("BBG000VHQTD1", 1000)   // серебро
             .build();
 
     public Map<String, Integer> getFigies() {
@@ -18,7 +22,12 @@ public class BYNbyKZTStrategy extends AInstrumentByInstrumentStrategy {
     }
 
     @Override
+    public float getMinimalDropPercent() {
+        return 0.45f;
+    }
+
+    @Override
     public boolean isEnabled() {
-        return false;
+        return true;
     }
 }
