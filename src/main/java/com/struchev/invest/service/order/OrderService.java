3a1af46322719cb0eed483e0d1a61e0c9cab9956
Roman Struchev <underpender@yandex.ru>
2024-04-21 16:50:07 +0200

    update price on order
    (cherry picked from commit 731c596a0093cfab3280dfe1376ec22fa4c7f8de)

--- src/main/java/com/struchev/invest/service/order/OrderService.java
+++ src/main/java/com/struchev/invest/service/order/OrderService.java
@@ -56,52 +56,75 @@ public class OrderService {
 
     @Transactional(Transactional.TxType.REQUIRES_NEW)
     public synchronized OrderDomainEntity buy(CandleDomainEntity candle, AStrategy strategy) {
+        // request to buy position
         var instrument = instrumentService.getInstrument(candle.getFigi());
+        var lots = strategy.getCount(candle.getFigi());
+        var response = tinkoffOrderAPI.buy(instrument, candle.getClosingPrice(), lots);
+
+        // update prices from response
+        pricesService.updatePrice(instrument.getFigi(), response.getPrice());
+
+        // save order to DB
         var currentPrices = pricesService.getCurrentPrices(strategy);
+        var orderDetails = OrderDetails.builder()
+                .currentPrices(currentPrices)
+                .build();
         var order = OrderDomainEntity.builder()
                 .currency(instrument.getCurrency())
                 .figi(instrument.getFigi())
                 .figiTitle(instrument.getName())
-                .purchasePrice(candle.getClosingPrice())
+                .purchasePrice(response.getPrice())
+                .purchaseCommission(response.getCommission())
                 .strategy(strategy.getName())
                 .purchaseDateTime(candle.getDateTime())
-                .lots(strategy.getCount(candle.getFigi()))
+                .lots(lots)
                 .purchaseCommission(BigDecimal.ZERO)
-                .details(OrderDetails.builder().currentPrices(currentPrices).build())
+                .details(orderDetails)
                 .build();
-
-        var result = tinkoffOrderAPI.buy(instrument, candle.getClosingPrice(), order.getLots());
-        order.setPurchaseCommission(result.getCommission());
-        order.setPurchasePrice(result.getPrice());
         order = orderRepository.save(order);
 
+        // update order list in memory
         orders.add(order);
+
         return order;
     }
 
     @Transactional(Transactional.TxType.REQUIRES_NEW)
     public synchronized OrderDomainEntity sell(CandleDomainEntity candle, AStrategy strategy) {
-        var instrument = instrumentService.getInstrument(candle.getFigi());
-        var currentPrices = pricesService.getCurrentPrices(strategy);
+        // find position to sell
         var order = findActiveByFigiAndStrategy(candle.getFigi(), strategy);
+        if (order == null) {
+            var s = String.format("No active order to sell by %s, %s", candle.getFigi(), strategy.getName());
+            throw new RuntimeException(s);
+        }
 
-        var result = tinkoffOrderAPI.sell(instrument, candle.getClosingPrice(), order.getLots());
+        // request to sell position
+        var instrument = instrumentService.getInstrument(candle.getFigi());
+        var response = tinkoffOrderAPI.sell(instrument, candle.getClosingPrice(), order.getLots());
+
+        // update prices from response
+        if (response.getPrice() != null) {
+            pricesService.updatePrice(instrument.getFigi(), response.getPrice());
+        }
+
+        // update order in DB
         order.setSellDateTime(OffsetDateTime.now());
-        order.setSellCommission(result.getCommission());
-        order.setSellPrice(result.getPrice() == null ? candle.getClosingPrice() : result.getPrice());
-        order.setSellProfit(result.getPrice().subtract(order.getPurchasePrice()));
-        if (order.getDetails() != null) {
-            order.getDetails().setSellPrices(currentPrices);
+        order.setSellCommission(response.getCommission());
+        order.setSellPrice(response.getPrice() == null ? candle.getClosingPrice() : response.getPrice());
+        order.setSellProfit(response.getPrice().subtract(order.getPurchasePrice()));
+        if (order.getDetails() != null && response.getPrice() != null) {
+            order.getDetails().setSellPrices(pricesService.getCurrentPrices(strategy));
         }
         order = orderRepository.save(order);
 
+        // update order list in memory
         var orderId = order.getId();
         var orderInList = orders.stream()
                 .filter(o -> o.getId().equals(orderId)).findFirst()
                 .orElseThrow();
-
         orders.remove(orderInList);
         orders.add(order);
+
         return order;
     }
 
