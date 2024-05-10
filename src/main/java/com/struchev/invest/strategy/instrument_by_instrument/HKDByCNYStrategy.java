d77fd8c64436c49e38bde695bd40be61a6db2ccd
Roman Struchev <underpender@yandex.ru>
2024-05-10 17:37:43 +0200

    Update description and strategies

--- src/main/java/com/struchev/invest/strategy/instrument_by_instrument/HKDByCNYStrategy.java
+++ src/main/java/com/struchev/invest/strategy/instrument_by_instrument/HKDByCNYStrategy.java
@@ -6,11 +6,11 @@ import org.springframework.stereotype.Component;
 import java.util.Map;
 
 @Component
-public class TUSDByTEURStrategy extends AInstrumentByInstrumentStrategy {
+public class HKDByCNYStrategy extends AInstrumentByInstrumentStrategy {
 
     private final Map<String, Integer> FIGIES = new ImmutableMap.Builder<String, Integer>()
-            .put("BBG000000000", 100) // TUSD
-            .put("BBG000000002", 100) // TEUR
+            .put("BBG0013HRTL0", 7000) // CNY
+            .put("BBG0013HSW87", 8000) // HKD
             .build();
 
     public Map<String, Integer> getFigies() {
