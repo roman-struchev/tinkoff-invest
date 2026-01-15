3a2ed27ac784dd6d63b6953b292a1beee0eeaf51
Roman Struchev <underpender@yandex.ru>
2026-01-15 20:51:40 +0100

    add multiple new strategies for CNY-based instruments and adjust force-to-sell duration to 5 days

---
+++ src/main/java/com/struchev/invest/strategy/instrument_by_instrument/CNYFByUSDFByEURFByGoldFDp02Strategy.java
@@ -0,0 +1,31 @@
+package com.struchev.invest.strategy.instrument_by_instrument;
+
+import com.google.common.collect.ImmutableMap;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Component
+public class CNYFByUSDFByEURFByGoldFDp02Strategy extends AInstrumentByInstrumentStrategy {
+
+    private final Map<String, Integer> FIGIES = new ImmutableMap.Builder<String, Integer>()
+            .put("FUTCNYRUBF00", 7) // CNY
+            .put("FUTUSDRUBF00", 1) // USDRUBF
+            .put("FUTEURRUBF00", 1) // EURRUBF
+            .put("FUTGLDRUBF00", 1) // GLDRUBF
+            .build();
+
+    public Map<String, Integer> getFigies() {
+        return FIGIES;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public float getMinimalDropPercent() {
+        return 0.2f;
+    }
+}
---
+++ src/main/java/com/struchev/invest/strategy/instrument_by_instrument/CNYFByUSDFDp02Strategy.java
@@ -0,0 +1,29 @@
+package com.struchev.invest.strategy.instrument_by_instrument;
+
+import com.google.common.collect.ImmutableMap;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Component
+public class CNYFByUSDFDp02Strategy extends AInstrumentByInstrumentStrategy {
+
+    private final Map<String, Integer> FIGIES = new ImmutableMap.Builder<String, Integer>()
+            .put("FUTCNYRUBF00", 7) // CNY
+            .put("FUTUSDRUBF00", 1) // USDRUBF
+            .build();
+
+    public Map<String, Integer> getFigies() {
+        return FIGIES;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public float getMinimalDropPercent() {
+        return 0.2f;
+    }
+}
