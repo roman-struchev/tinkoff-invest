bed173d690f53e0068854e5a7df78c7ee27fe9ff
Roman Struchev <underpender@yandex.ru>
2026-01-29 07:38:12 +0100

    add new strategies for CRH6BySiH6ByEuH6 with varying minimal drop percentages

---
+++ src/main/java/com/struchev/invest/strategy/instrument_by_instrument/CRH6BySiH6ByEuH6Dp02Strategy.java
@@ -0,0 +1,30 @@
+package com.struchev.invest.strategy.instrument_by_instrument;
+
+import com.google.common.collect.ImmutableMap;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Component
+public class CRH6BySiH6ByEuH6Dp02Strategy extends AInstrumentByInstrumentStrategy {
+
+    private final Map<String, Integer> FIGIES = new ImmutableMap.Builder<String, Integer>()
+            .put("CRH6", 7) // CNY
+            .put("SiH6", 1) // USD
+            .put("EuH6", 1) // EUR
+            .build();
+
+    public Map<String, Integer> getFigies() {
+        return FIGIES;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public float getMinimalDropPercent() {
+        return 0.2f;
+    }
+}
---
+++ src/main/java/com/struchev/invest/strategy/instrument_by_instrument/CRH6BySiH6ByEuH6Strategy.java
@@ -0,0 +1,25 @@
+package com.struchev.invest.strategy.instrument_by_instrument;
+
+import com.google.common.collect.ImmutableMap;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Component
+public class CRH6BySiH6ByEuH6Strategy extends AInstrumentByInstrumentStrategy {
+
+    private final Map<String, Integer> FIGIES = new ImmutableMap.Builder<String, Integer>()
+            .put("CRH6", 7) // CNY
+            .put("SiH6", 1) // USD
+            .put("EuH6", 1) // EUR
+            .build();
+
+    public Map<String, Integer> getFigies() {
+        return FIGIES;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
